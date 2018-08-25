b70ac073d4737f3ac3adeb073e50c744c2c9f88a
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-25 13:33:56 -0400

    fat arrow function syntax

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,18 +5,17 @@ export default class TodoComponent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            todos: ['drink tea', 'walk the dog', 'wash the dishes', 'cut the cheese', 'take a nap']
+            todos: ['drink tea', 'wash the dishes', 'cut the cheese', 'take a nap']
         };
     }
     //getInitialState
     render(){
 		var todos = this.state.todos;
-        todos = todos.map(function(item, index){
-            return(
-                    <li>`{item}...!`</li>
-            );
-        });
-        return(
+        todos = todos.map((item, index) => {
+			return <li>`{item}...!`</li>
+		});
+
+		return(
             <div id="todo-list">
                 <p>React stuff goes here...</p>
 				<ul>{todos}</ul>
