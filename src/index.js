5b44f15044ac88baefa06cf37b7dfff0cd6ad5e4
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-25 12:46:59 -0400

    props w/ es6 -not jsx- &wp1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { TodoComponent } from './app/ToDo';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-render(
-	<TodoComponent msg="who cut the cheese"/>,
-	document.getElementById('todo-wrapper')
-)
+//Create a component
+var TodoComponent = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <p><strong>Cheese name: </strong> {this.props.cheese.name}</p>
+                <p><strong>Cheese smell factor: </strong> {this.props.cheese.smellFactor}</p>
+                <p><strong>Cheese price: </strong>£{this.props.cheese.price}</p>
+            </div>
+        );
+    }
+});
+
+var myCheese = {name: 'Camembert', smellFactor: 'Extreme pong', price: 3.50 };
+
+ReactDOM.render(<TodoComponent cheese={myCheese} />, document.getElementById('todo-wrapper'));
