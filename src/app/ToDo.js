a5dd28ff77e8b508a3166fe57c94bbb01462805e
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-25 12:20:54 -0400

    webpack 4 & es6 class

---
+++ src/app/ToDo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a component using ES6 Class Syntax
+export class TodoComponent extends React.Component{
+    render(){
+        return(
+            <h1>Using ES6 Classes</h1>
+        );
+    }
+};
