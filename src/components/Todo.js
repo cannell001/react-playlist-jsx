cee26c990e70a634b96b60482f54e985bc68e21e
Jeremy Cannell <jcannell007@hotmail.com>
2018-08-25 13:24:46 -0400

    Map function demo with template literals

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,19 +5,21 @@ export default class TodoComponent extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            todos: ['wash dishes', 'cut the cheese', 'take a nap']
+            todos: ['drink tea', 'walk the dog', 'wash the dishes', 'cut the cheese', 'take a nap']
         };
     }
     //getInitialState
     render(){
+		var todos = this.state.todos;
+        todos = todos.map(function(item, index){
+            return(
+                    <li>`{item}...!`</li>
+            );
+        });
         return(
             <div id="todo-list">
                 <p>React stuff goes here...</p>
-				<ul>
-					<li>{this.state.todos[0]}</li>
-					<li>{this.state.todos[1]}</li>
-					<li>{this.state.todos[2]}</li>
-				</ul>
+				<ul>{todos}</ul>
             </div>
         );
     } //render
